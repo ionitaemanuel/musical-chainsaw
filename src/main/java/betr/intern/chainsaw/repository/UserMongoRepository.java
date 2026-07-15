d826bb5e016805a44157d9377f24cc82dc395575
ionitaemanuel <293794112+ionitaemanuel@users.noreply.github.com>
2026-07-15 17:08:51 +0300

    refactor(): resolve deprecated files added by master branch

--- src/main/java/betr/intern/chainsaw/repository/UserMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package betr.intern.chainsaw.repository;
-
-import betr.intern.chainsaw.model.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserMongoRepository extends MongoRepository<User, String> {
-    User findByEmail(String email);
-}
